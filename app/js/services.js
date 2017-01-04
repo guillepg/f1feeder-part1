ca066fa313e54442650986486a8acf6f02415baf
guillepg <guillepg@outlook.es>
2017-01-04 12:10:39 +0100

    temporada modificada

--- app/js/services.js
+++ app/js/services.js
@@ -4,25 +4,25 @@ angular.module('F1FeederApp.services', [])
     var ergastAPI = {};
 
     ergastAPI.getDrivers = function() {
-      return $http({
-        method: 'JSONP', 
-        url: 'http://ergast.com/api/f1/2016/driverStandings.json?callback=JSON_CALLBACK'
-      });
-    }
+        return $http({
+            method: 'JSONP', 
+            url: 'http://ergast.com/api/f1/2016/driverStandings.json?callback=JSON_CALLBACK'
+        });
+    };
 
     ergastAPI.getDriverDetails = function(id) {
-      return $http({
-        method: 'JSONP', 
-        url: 'http://ergast.com/api/f1/2016/drivers/'+ id +'/driverStandings.json?callback=JSON_CALLBACK'
-      });
-    }
+        return $http({
+            method: 'JSONP', 
+            url: 'http://ergast.com/api/f1/2016/drivers/'+ id +'/driverStandings.json?callback=JSON_CALLBACK'
+        });
+    };
 
     ergastAPI.getDriverRaces = function(id) {
-      return $http({
-        method: 'JSONP', 
-        url: 'http://ergast.com/api/f1/2016/drivers/'+ id +'/results.json?callback=JSON_CALLBACK'
-      });
-    }
+        return $http({
+            method: 'JSONP', 
+            url: 'http://ergast.com/api/f1/2016/drivers/'+ id +'/results.json?callback=JSON_CALLBACK'
+        });
+    };
 
     return ergastAPI;
   });
